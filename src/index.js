d189a5c326a34f3a0880189559c60c6593d6dffc
Carlos Kazuo Mochizuki Ishizaka <carlos.ishizaka@dafiti.com.br>
2020-05-30 05:22:30 -0300

    feat: 🎸 added minireset.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'minireset.css/minireset.min.css'
 import './index.css'
 import App from './App.js'
 import * as serviceWorker from './serviceWorker.js'
